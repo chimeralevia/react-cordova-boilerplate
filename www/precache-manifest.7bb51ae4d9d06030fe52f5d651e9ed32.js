94e81b820b4c75362772de720c2eb90da02732d0
Halil <h.i.tuerk@googlemail.com>
2020-04-07 13:16:12 +0200

    initial commit

---
+++ www/precache-manifest.7bb51ae4d9d06030fe52f5d651e9ed32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98687a69059e5d454107b41ba856018",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6dd7cc58193cf94bc8d4",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f6a7474442d37483deb1",
+    "url": "./static/js/2.cd87f2cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cd87f2cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd7cc58193cf94bc8d4",
+    "url": "./static/js/main.587e6cef.chunk.js"
+  },
+  {
+    "revision": "34ad57dde141a9c0af99",
+    "url": "./static/js/runtime-main.1b7d8475.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
